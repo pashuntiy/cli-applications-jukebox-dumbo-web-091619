c34a83c1c1bf92e8b76563e07c9b5c4cc93515f6
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-04 20:56:55 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,3 +25,14 @@ def list(songs)
   end
 end
 
+def play(songs)
+  puts "Please enter a song name or number:"
+  response = gets.chomp
+  if response.to_i >= 1 && response.to_i <= songs.length
+    puts "Playing #{songs[response.to_i-1]}"
+  elsif songs.include?(response)
+    puts "Playing #{songs.find{|song| song == response}}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
