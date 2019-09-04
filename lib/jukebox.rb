324fc0be8416c7df193f1611d92973fb06e3a7a1
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-04 20:47:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,9 +12,10 @@ songs = [
 
 
 def help
-  puts "I accept the following commands:/n
-  - help : displays this help message/n
-  - list : displays a list of songs you can play/n
-  - play : lets you choose a song to play/n
+  puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
   - exit : exits this program"
 end
+
